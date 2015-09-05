9624ce580a3239f2d2f8f7e50283b7e5ba0bfbcb
ander <andermaco@gmail.com>
2015-09-05 12:12:42 +0200

    Added rotation menu setting

--- app/src/main/java/com/example/movies/MainActivityFragment.java
+++ app/src/main/java/com/example/movies/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package com.example.movies;
 
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -26,16 +32,25 @@ import java.util.ArrayList;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final String TAG = MainActivityFragment.class.getSimpleName();
     private ArrayList<MovieDataParcelable> mArrayList;
     private static final String MOVIE_KEY = "MOVIES";
+    private static final String SORT_KEY = "SORT";
     protected ImageAdapter mImageAdapter;
+    private Menu sMenu;
+    private SharedPreferences mPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mArrayList = new ArrayList<MovieDataParcelable>();
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        setHasOptionsMenu(true);
         if (savedInstanceState == null) {
-            loadMovies();
+            String sort_type = getResources().getString(R.string.dbapiurl_popularity);
+            loadMovies(sort_type);
+            mPreferences.edit().putString(SORT_KEY,
+                    getResources().getString(R.string.most_popular)).apply();
         }
     }
 
@@ -58,19 +73,54 @@ public class MainActivityFragment extends Fragment {
         outState.putParcelableArrayList(MOVIE_KEY, mArrayList);
     }
 
-    public void loadMovies () {
-        FetchDBMovieTask fetchDBMovieTask = new FetchDBMovieTask();
-        fetchDBMovieTask.execute();
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment, menu);
+        if (mPreferences.getString(SORT_KEY, "").equals(getResources().getString(R.string.most_popular))) {
+            menu.findItem(R.id.highest_rated).setVisible(true);
+            menu.findItem(R.id.most_popular).setVisible(false);
+        } else {
+            menu.findItem(R.id.most_popular).setVisible(true);
+            menu.findItem(R.id.highest_rated).setVisible(false);
+        }
+        this.sMenu = menu;
     }
 
-    private class FetchDBMovieTask extends AsyncTask<Void, Void, ArrayList<MovieDataParcelable>> {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.most_popular:
+                item.setVisible(false);
+                sMenu.findItem(R.id.highest_rated).setVisible(true);
+                mPreferences.edit().putString(SORT_KEY,
+                        getResources().getString(R.string.most_popular)).apply();
+                loadMovies(getResources().getString(R.string.dbapiurl_popularity));
+                return true;
+            case R.id.highest_rated:
+                item.setVisible(false);
+                sMenu.findItem(R.id.most_popular).setVisible(true);
+                mPreferences.edit().putString(SORT_KEY,
+                        getResources().getString(R.string.highest_rated)).apply();
+                loadMovies(getResources().getString(R.string.dbapiurl_rate));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void loadMovies (String sort) {
+        FetchDBMovieTask fetchDBMovieTask = new FetchDBMovieTask();
+        fetchDBMovieTask.execute(sort);
+    }
+
+    private class FetchDBMovieTask extends AsyncTask<String, Void, ArrayList<MovieDataParcelable>> {
         private final String TAG = FetchDBMovieTask.class.getSimpleName();
 
         @Override
-        protected ArrayList<MovieDataParcelable> doInBackground(Void... voids) {
+        protected ArrayList<MovieDataParcelable> doInBackground(String... strings) {
             Log.i(TAG, "doInBackground");
             mArrayList = new ArrayList<MovieDataParcelable>();
-            JSONArray json_array = getMovieData().optJSONArray("results");
+            JSONArray json_array = getMovieData(strings[0]).optJSONArray("results");
             for (int i=0; i<json_array.length(); i++) {
                 try {
                     JSONObject jsonObject = json_array.getJSONObject(i);
@@ -83,13 +133,17 @@ public class MainActivityFragment extends Fragment {
             return mArrayList;
         }
 
-        protected JSONObject getMovieData () {
+        protected JSONObject getMovieData (String sort) {
             HttpURLConnection connection = null;
             BufferedReader reader = null;
             StringBuffer buffer = null;
             try {
-                URL url = new URL(getResources().getString(R.string.dbapiurl)
-                        .concat(getResources().getString(R.string.api_key)));
+                Resources resources = getActivity().getResources();
+                URL url = new URL(resources.getString(R.string.dbapiurl)
+//                    .concat(resources.getString(R.string.dbapiurl_popularity)
+                    .concat(sort
+                    .concat(resources.getString(R.string.dbapikey_param)
+                    .concat(resources.getString(R.string.dbapikey)))));
 
                 connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
